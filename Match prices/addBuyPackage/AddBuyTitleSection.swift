06cf30f11b962ad64beb9a4a08d85876f91eabdc
nolit1 <kj2806@mail.ru>
2020-02-17 10:39:43 +0300

    рабочая сборка

---
+++ Match prices/addBuyPackage/AddBuyTitleSection.swift
@@ -0,0 +1,24 @@
+//
+//  AddBuyTitleSection.swift
+//  Match prices
+//
+//  Created by Евгений Конев on 17.02.2020.
+//  Copyright © 2020 Nolit. All rights reserved.
+//
+
+import UIKit
+
+class AddBuyTitleSection: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
