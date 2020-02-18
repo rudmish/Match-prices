5695aa517eb6aa23114f54c40a8da0b84e83aff2
nolit1 <kj2806@mail.ru>
2020-02-19 00:49:50 +0300

    swift 5 -> swift 4.2

---
+++ Pods/Target Support Files/Pods-Match prices-Match pricesUITests/Pods-Match prices-Match pricesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Match_prices_Match_pricesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Match_prices_Match_pricesUITests
+@end
---
+++ Pods/Target Support Files/Pods-Match pricesTests/Pods-Match pricesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Match_pricesTests : NSObject
+@end
+@implementation PodsDummy_Pods_Match_pricesTests
+@end
